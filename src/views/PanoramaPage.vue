892ed5e32b3f3b4598f389e91c78e448dea0c1de
Arild Matsson <arild.matsson@gu.se>
2022-05-23 16:21:12 +0200

    Viewer template to Vue (pannellum broken)

--- src/views/PanoramaPage.vue
+++ src/views/PanoramaPage.vue
@@ -1,35 +1,85 @@
 <script setup>
 import { remoteObject } from "../strapi";
+import "@/assets/item.css";
+import "@/assets/pannellum/pannellum.js";
+import "@/assets/pannellum/pannellum.css";
 
 const props = defineProps(["id"]);
 
 const image = remoteObject(`images/${props.id}`);
+
+const pannellum = window.pannellum;
+
+const personName = (person) =>
+  [person.firstname, person.lastname].filter(Boolean).join(" ");
 </script>
 
 <template>
   <div v-if="image.id">
-    <div>
-      <img
-        :src="`https://sodrarada.dh.gu.se/api/${image.image.formats.large.url}`"
-        alt=""
-      />
+    <div id="item-top-image" style="display: none"></div>
+    <div id="item-top-video" style="display: none"></div>
+
+    <div id="item-top-pano">
+      <div id="panorama">
+        <img :src="'https://sodrarada.dh.gu.se/api' + image.image.url" alt="" />
+      </div>
+      <!-- {{
+        pannellum.viewer("panorama", {
+          type: "equirectangular",
+          autoLoad: true,
+          autoRotate: false,
+          yaw: -0,
+          pitch: 30,
+          showZoomCtrl: false,
+          panorama: "https://sodrarada.dh.gu.se/api" + image.image.url,
+        })
+      }} -->
     </div>
-    <div>{{ image.subject }}</div>
-    <div>{{ image.description }}</div>
-    <div>{{ image.creation }}</div>
-    <div>{{ image.stage }}</div>
-    <div>{{ image.source }}</div>
-    <div>{{ image.type }}</div>
-    <div>{{ image.iiif }}</div>
-    <div>{{ image.isPanorama }}</div>
-    <div>
-      {{ image.date.year }}, {{ image.date.approximate }},
-      {{ image.date.uncertain }}
+
+    <div v-if="pannellum" id="metadata">
+      <a href="../snowhill.html">
+        <div id="item-back"></div>
+      </a>
+
+      <div class="section-title">{{ image.subject }}</div>
+
+      <div class="meta-article-solo" style="margin-top: 20px">
+        {{ image.description }}
+      </div>
+
+      <div class="section-title">Specifikationer</div>
+
+      <div class="meta-container">
+        Datering: <span>{{ image.date && image.date.year }}</span> <br />
+        Upplösning:
+        <span>{{ image.image.width }} x {{ image.image.height }}</span> <br />
+        Skapare: <span>{{ image.creator.map(personName).join(", ") }}</span>
+        <br />
+        Licens: <span>Creative Commons 4.0 / PD</span> <br />
+      </div>
+
+      <div id="filter-container">
+        <div
+          v-for="keyword in image.keywords"
+          :key="keyword.id"
+          class="filtertag"
+        >
+          {{ keyword.label }}
+        </div>
+      </div>
+
+      <a :href="'https://sodrarada.dh.gu.se/api' + image.image.url">
+        <div class="download-container">
+          <div class="download-button"></div>
+          <div class="download-label">Ladda ner bild</div>
+        </div>
+      </a>
+      <br />
+
+      <div class="section-title" style="margin-top: 70px">Relaterat</div>
+
+      <div class="related-gallery"></div>
     </div>
-    <div>{{ image.image.name }}</div>
-    <div>{{ image.image.alternativeText }}</div>
-    <div>{{ image.image.width }}</div>
-    <div>{{ image.image.height }}</div>
   </div>
 </template>
 
